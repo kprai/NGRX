0f5f9c02d8209a0b7f26acf006e3ce3711f40ffa
kprai <53602435+kprai@users.noreply.github.com>
2020-02-26 13:15:52 +0000

    Add files via upload

---
+++ src/app/messages/message-start/message-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-start',
+  templateUrl: './message-start.component.html',
+  styleUrls: ['./message-start.component.css']
+})
+export class RecipeStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
