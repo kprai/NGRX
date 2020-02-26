3d58545f9b3d28d0a29ab4d6bc28b50ea47322a8
kprai <53602435+kprai@users.noreply.github.com>
2020-02-26 13:05:58 +0000

    Add files via upload

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducer';
+import * as RecipeActions from '../messages/store/recipe.actions';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
+
+  constructor(
+    private store: Store<fromApp.AppState>
+  ) {}
+
+  ngOnInit() {
+  }
+
+  onSaveData() {
+    // this.dataStorageService.storeRecipes();
+    this.store.dispatch(new RecipeActions.StoreRecipes());
+  }
+
+  onFetchData() {
+    // this.dataStorageService.fetchRecipes().subscribe();
+    this.store.dispatch(new RecipeActions.FetchRecipes());
+  }
+
+  //onLogout() { this.store.dispatch(new AuthActions.Logout());}
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+}
